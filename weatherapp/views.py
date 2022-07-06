283f55d16cce4149fd97ca742132251b7f0356fb
yakupkose3123 <yakupkose3123@gmail.com>
2022-07-06 18:58:41 +0300

    Data pulled from API

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from decouple import config
+import requests
+from pprint import pprint #print yazınca gelen veriyi daha düzenli göstermek için
 
-def home(request):    
-    return render(request, "weatherapp/home.html")
+def home(request):  
+    API_KEY = config('API_KEY')  
+    city = "Yozgat"
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url) #request metodunu kullanaarak bu urlden bilgileri çek
+    content = response.json() #json veriyi dictionary e çevirdik
+    # pprint(content)
+    context = {
+        'city' : content['name'],
+        'temp' : content['main']['temp'],
+        'icon' : content['weather'][0]['icon'],
+        'desc' : content['weather'][0]['description'],
+    }
+    
+    print
+    return render(request, "weatherapp/home.html", context)
